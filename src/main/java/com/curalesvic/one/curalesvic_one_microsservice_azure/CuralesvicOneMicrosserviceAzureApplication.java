6bdda498b8019e66b603fc1adc8a5ae08ab4fa30
Victor Gabriel Vieira Fechine Tavares <miranhaomi@gmail.com>
2024-08-16 18:05:26 -0300

    subindo repositório

---
+++ src/main/java/com/curalesvic/one/curalesvic_one_microsservice_azure/CuralesvicOneMicrosserviceAzureApplication.java
@@ -0,0 +1,13 @@
+package com.curalesvic.one.curalesvic_one_microsservice_azure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CuralesvicOneMicrosserviceAzureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CuralesvicOneMicrosserviceAzureApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/curalesvic/one/curalesvic_one_microsservice_azure/CuralesvicOneMicrosserviceAzureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.curalesvic.one.curalesvic_one_microsservice_azure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CuralesvicOneMicrosserviceAzureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
